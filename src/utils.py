01170bac10e773fae7b57b33047e122925192962
Tianyi Zhang <63892561+tianyi-zhang23@users.noreply.github.com>
2021-12-02 17:19:09 -0500

    add backtranslation

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,27 @@
 import random
 import torch
 from torch.utils.data import Dataset
+import nlpaug.augmenter.word as naw
+from enum import Enum
+
+
+class Language(Enum):
+    German = 0
+
+
+def backtranslation(text: str, lang: Language) -> str:
+    models = {
+        Language.German: ('facebook/wmt19-en-de', 'facebook/wmt19-de-en')
+    }
+    aug = naw.BackTranslationAug(
+        from_model_name=models[lang][0],
+        to_model_name=models[lang][1])
+    return aug.augment(text)
+
 
 def random_swap(words, p):
-    n = int(len(words)*p)
-    if n<1:
+    n = int(len(words) * p)
+    if n < 1:
         return words
     else:
         words = words.split()
@@ -20,6 +37,8 @@ def swap(words):
     words[idx[0]], words[idx[1]] = words[idx[1]], words[idx[0]]
 
     return words
+
+
 '''
 class PandasDataset(Dataset):
     def __init__(self, dataframe):
