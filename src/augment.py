e4224f1878c51f2ff7196223b6d416a51e43d4e0
Tianyi ZHANG <tzhang74@teaching.CS.McGill.CA>
2021-12-05 01:32:52 -0500

    add arg for skipping chunks

--- src/augment.py
+++ src/augment.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import torchtext
+#import torchtext # uncomment to run load_imdb_online
 from tqdm import tqdm
 from utils import Language, backtranslation, contextual_word_embeddings
 
@@ -18,14 +18,14 @@ def save_imdb_original():
     test_df.to_pickle("./imdb_test_df_original.pkl")
 
 
-def save_imdb_backtrans(lang):
-    for i, chunk in enumerate(np.array_split(pd.read_pickle("./imdb_train_df_original.pkl"), 250)):
+def save_imdb_backtrans(lang, begin_chunk_idx = 0):
+    for i, chunk in list(enumerate(np.array_split(pd.read_pickle("./imdb_train_df_original.pkl"), 250)))[begin_chunk_idx:]:
         chunk['text'] = chunk.progress_apply(lambda row: backtranslation(row['text'], lang), axis=1)
         chunk.to_pickle(f"./imdb_train_df_augd_backtrans_{lang}/imdb_train_df_augd_backtrans_{lang}_{i}.pkl")
 
 
-def save_imdb_contextual_word_embeddings():
-    for i, chunk in enumerate(np.array_split(pd.read_pickle("./imdb_train_df_original.pkl"), 250)):
+def save_imdb_contextual_word_embeddings(begin_chunk_idx = 0):
+    for i, chunk in list(enumerate(np.array_split(pd.read_pickle("./imdb_train_df_original.pkl"), 250)))[begin_chunk_idx:]:
         chunk['text'] = chunk.progress_apply(contextual_word_embeddings, axis=1)
         chunk.to_pickle(f"./imdb_train_df_augd_contextual_word_embeddings"
                            f"/imdb_train_df_augd_contextual_word_embeddings_{i}.pkl")
@@ -34,4 +34,4 @@ def save_imdb_contextual_word_embeddings():
 if __name__ == '__main__':
     tqdm.pandas()
     # save_imdb_original()
-    save_imdb_backtrans(Language.German)
+    #save_imdb_backtrans(Language.German)
