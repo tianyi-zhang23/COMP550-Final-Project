baf651499e6ac4c15fa0fdc29e4a8ce82f57ffab
Tianyi Zhang <tianyi.zhang5@mail.mcgill.ca>
2021-12-06 10:49:28 -0500

    new chunks; better env preparation

--- src/prepare_env.sh
+++ src/prepare_env.sh
@@ -1,10 +1,7 @@
 apt update
-apt install software-properties-common
+apt -y install software-properties-common
 add-apt-repository ppa:deadsnakes/ppa
-apt install python3.9
-apt install python3.9-distutils
+apt -y install python3.9 python3.9-distutils
 python3.9 -m pip install six
-python3.9 -m pip install --upgrade setuptools
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install --upgrade distlib
+python3.9 -m pip install --upgrade setuptools pip distlib
 python3.9 -m pip install pandas nltk nlpaug torch transformers
