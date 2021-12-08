1d8a463e6ab7ec4fe994caf23d5d7ebb9dd12380
Tianyi Zhang <63892561+tianyi-zhang23@users.noreply.github.com>
2021-12-08 15:36:36 -0500

    just one chunk for word embeddings

--- src/augment.py
+++ src/augment.py
@@ -18,21 +18,22 @@ def save_imdb_original():
     test_df.to_pickle("./imdb_test_df_original.pkl", protocol=4)
 
 
-def save_imdb_backtrans(lang, begin_chunk_idx = 0):
+def save_imdb_backtrans(lang, begin_chunk_idx = 0, device='cuda'):
     for i, chunk in list(enumerate(np.array_split(pd.read_pickle("./imdb_train_df_original.pkl"), 250)))[begin_chunk_idx:]:
-        chunk['text'] = chunk.progress_apply(lambda row: backtranslation(row['text'], lang), axis=1)
+        chunk['text'] = chunk.progress_apply(lambda row: backtranslation(row['text'], lang, device=device), axis=1)
         chunk.to_pickle(f"./imdb_train_df_augd_backtrans_{lang}/imdb_train_df_augd_backtrans_{lang}_{i}.pkl")
 
 
-def save_imdb_contextual_word_embeddings(begin_chunk_idx = 0):
-    for i, chunk in list(enumerate(np.array_split(pd.read_pickle("./imdb_train_df_original.pkl"), 250)))[begin_chunk_idx:]:
-        chunk['text'] = chunk.progress_apply(contextual_word_embeddings, axis=1)
-        chunk.to_pickle(f"./imdb_train_df_augd_contextual_word_embeddings"
-                           f"/imdb_train_df_augd_contextual_word_embeddings_{i}.pkl")
+# Just one chunk since this is relatively fast
+def save_imdb_contextual_word_embeddings():
+    df = pd.read_pickle("./imdb_train_df_original.pkl")
+    df['text'] = df.progress_apply(lambda row: contextual_word_embeddings(row['text']), axis=1)
+    df.to_pickle(f"./imdb_train_df_augd_contextual_word_embeddings.pkl")
+
 
 
 if __name__ == '__main__':
     tqdm.pandas()
     # save_imdb_original()
-    #save_imdb_backtrans(Language.German)
+    #save_imdb_backtrans(Language.German, begin_chunk_idx=99, device='cpu')
     save_imdb_contextual_word_embeddings()
